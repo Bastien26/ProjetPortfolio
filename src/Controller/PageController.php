43a30e85b82fdb61ef62f715f3eb09dacdf33d6e
Bastien26 <bastien.comte@etu.univ-st-etienne.fr>
2023-01-20 16:54:23 +0100

    Init

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,10 +8,38 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/test', name: 'app_page')]
+    #[Route('/', name: 'app_page')]
     public function index(): Response
     {
         return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/CV', name: 'CV')]
+    public function CV(): Response
+    {
+        return $this->render('page/CV.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/Portfolio', name: 'Portfolio')]
+    public function Portfolio(): Response
+    {
+        return $this->render('page/Portfolio.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/CDU', name: 'CDU')]
+    public function CDU(): Response
+    {
+        return $this->render('page/Portfolio.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/En_savoir_plus', name: 'ESP')]
+    public function ESP(): Response
+    {
+        return $this->render('page/ESP.html.twig', [
             'controller_name' => 'PageController',
         ]);
     }
